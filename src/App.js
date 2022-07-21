f9eef6cb6b6966301c2a11adbe410fff0fc917be
Thiago Henrique <thiagoedusan5.11@outlook.com>
2022-07-21 19:06:38 -0300

    modificando readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Meu primeiro deploy</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2YCb1tB67O22GuI1mH23kLp3xvo9p5FS4nw&usqp=CAU"/>
     </div>
   );
 }
