a3ae06f2f8ef11c7440271d3c3dec37d5d61e17a
Thiago Henrique <thiagoedusan5.11@outlook.com>
2022-07-21 19:48:01 -0300

    atualizando

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Meu primeiro deploy</h1>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2YCb1tB67O22GuI1mH23kLp3xvo9p5FS4nw&usqp=CAU"/>
+      <img src="https://media.giphy.com/media/6Z3D5t31ZdoNW/giphy.gif"/>
       <p>Amiente de teste</p>
     </div>
   );
