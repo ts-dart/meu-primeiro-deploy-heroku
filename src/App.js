2cb7745ceb4659fea9f0e9dfa262a81ad552c62e
Thiago Henrique <thiagoedusan5.11@outlook.com>
2022-07-21 19:34:26 -0300

    Subindo ambiente de teste

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <h1>Meu primeiro deploy</h1>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2YCb1tB67O22GuI1mH23kLp3xvo9p5FS4nw&usqp=CAU"/>
-      <p>Atualizando</p>
+      <p>Amiente de teste</p>
     </div>
   );
 }
