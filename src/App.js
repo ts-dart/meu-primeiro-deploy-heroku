a867794e5043a1dccfa920c74786460b5420c6da
Thiago Henrique <thiagoedusan5.11@outlook.com>
2022-07-22 12:50:22 -0300

    Atualizando

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Meu primeiro deploy</h1>
-      <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fcoodesh.com%2Fblog%2Fwp-content%2Fuploads%2F2021%2F08%2Fo-que-e-deploy-redes-1152x648.png&imgrefurl=https%3A%2F%2Fcoodesh.com%2Fblog%2Fdicionario%2Fo-que-e-deploy%2F&tbnid=ao9cBlnMXtIwlM&vet=12ahUKEwjd6erq6oz5AhV6vJUCHUWeA3wQMygCegUIARDRAQ..i&docid=A1sg9WrSgbTJyM&w=1152&h=648&q=deploy&ved=2ahUKEwjd6erq6oz5AhV6vJUCHUWeA3wQMygCegUIARDRAQ"/>
+      <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmabittar.github.io%2Fassets%2Fimg%2Fdeploy.jpg&imgrefurl=https%3A%2F%2Fmabittar.github.io%2FModel-deploy%2F&tbnid=WL3xyBG-vpwHVM&vet=12ahUKEwjd9KbA7Iz5AhUXOLkGHbSQCU8QMygHegUIARDVAQ..i&docid=4dusa2Vxh8-uDM&w=317&h=159&q=deploy&ved=2ahUKEwjd9KbA7Iz5AhUXOLkGHbSQCU8QMygHegUIARDVAQ"/>
     </div>
   );
 }
