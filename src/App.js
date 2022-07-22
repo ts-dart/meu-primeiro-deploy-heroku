df929d53fb63bac673db0b987802d658626af2ea
Thiago Henrique <thiagoedusan5.11@outlook.com>
2022-07-22 12:46:20 -0300

    Atualizando

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Meu primeiro deploy</h1>
-      <img src="https://media.giphy.com/media/6Z3D5t31ZdoNW/giphy.gif"/>
-      <p>Amiente de teste</p>
+      <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fcoodesh.com%2Fblog%2Fwp-content%2Fuploads%2F2021%2F08%2Fo-que-e-deploy-redes-1152x648.png&imgrefurl=https%3A%2F%2Fcoodesh.com%2Fblog%2Fdicionario%2Fo-que-e-deploy%2F&tbnid=ao9cBlnMXtIwlM&vet=12ahUKEwjd6erq6oz5AhV6vJUCHUWeA3wQMygCegUIARDRAQ..i&docid=A1sg9WrSgbTJyM&w=1152&h=648&q=deploy&ved=2ahUKEwjd6erq6oz5AhV6vJUCHUWeA3wQMygCegUIARDRAQ"/>
     </div>
   );
 }
